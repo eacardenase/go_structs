79b9e1f1a01d430bb0d5de65ccee7e1b83cd61bf
Edwin Cardenas <eacardenase@gmail.com>
2024-11-05 18:53:56 -0500

    feat: add custom struct

--- structs.go
+++ structs.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
 
 func main() {
-	fmt.Println("Go Structs")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	appUser := user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	// appUser := new(user) // creates a pointer
+	// appUser.firstName = firstName
+	// appUser.lastName = lastName
+	// appUser.birthdate = birthdate
+	// appUser.createdAt = time.Now()
+
+	outputUserDetails(appUser)
+}
+
+func outputUserDetails(user user) {
+	fmt.Println(user.firstName, user.lastName, user.birthdate)
+}
+
+func getUserData(promptText string) string {
+	var value string
+
+	fmt.Print(promptText)
+	fmt.Scan(&value)
+
+	return value
 }
