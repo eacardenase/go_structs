41bfb187914f2622a67aacf18a7ef0207ba9aa56
Edwin Cardenas <eacardenase@gmail.com>
2024-11-05 20:58:21 -0500

    refactor: extract user struct into its own package

--- structs.go
+++ structs.go
@@ -1,46 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"github.com/eacardenase/go_structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthdate string
-	createdAt time.Time
-}
-
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("new user validation failed")
-	}
-
-	return &user{
-		firstName,
-		lastName,
-		birthdate,
-		time.Now(),
-	}, nil
-}
-
-func (u *user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthdate)
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println(err)
@@ -48,9 +19,9 @@ func main() {
 		return
 	}
 
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
